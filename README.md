c56c2d57c75d29ba86f44ed28c8a2bd030cc5ea8
João Gabriel <joao.royosilva@gmail.com>
2019-07-16 21:09:00 -0400

    Correçõa da versão do react native e css

--- README.md
+++
@@ -1,1 +0,0 @@
-# bootcamp-rocketseat-desafio-7
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.View`
   padding: 10px;
   background: #fff;
   border-radius: 4px;
-  margin: 15px;
+  margin: 60px 15px;
 `;
 
 export const Products = styled.View``;
